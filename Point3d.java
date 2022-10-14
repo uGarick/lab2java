ab35dde220319ceb5e11ba5924eaefa538a17847
Igor <igor.gaz2004@gmail.com>
2022-10-14 11:53:47 +0300

    v3.0

--- Point3d.java
+++ Point3d.java
@@ -1,10 +1,8 @@
 public class Point3d extends Point2d {
-    Point2d coords = new Point2d();
     private double zCoord;
 
     public Point3d(double x, double y, double z) {
-        coords.setX(x);
-        coords.setY(y);
+        super(x, y);
         zCoord = z;
     }
     public Point3d() {
@@ -20,8 +18,8 @@ public class Point3d extends Point2d {
     }
 
     public double distanceTo(double nX, double nY, double nZ) {
-        double oX = coords.getX();
-        double oY = coords.getY();
+        double oX = getX();
+        double oY = getY();
         double oZ = getZ();
         double result = Math.sqrt(Math.pow(nX - oX, 2) + Math.pow(nY - oY, 2) + Math.pow(nZ - oZ, 2));
         result = (Math.ceil(result*100))/100;
